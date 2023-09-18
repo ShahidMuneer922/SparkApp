25782763e72b6541d8ebcefeee87cf1c1e0c851f
Shahid Muneer <shahidmuneerawan@gmail.com>
2023-09-18 20:54:16 +0500

    fix: cors error

--- middeleware.js
+++ middeleware.js
@@ -6,10 +6,10 @@ const Middle = async (req, res, next) => {
     // console.log("QUERY-PARAMS", req.query);
     // console.log("HEADERS", req.headers);
     // console.log("URL", req.url);
-    // res.setHeader("Access-Control-Allow-Origin", "*");
-    // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    // res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
     next();
   } catch (err) {
     console.log(err);
