4d4d9046065acf7c1b253941a326ddd012fcc296
Shahid Muneer <shahidmuneerawan@gmail.com>
2023-09-18 16:50:29 +0500

    added err

--- controller/team/team.js
+++ controller/team/team.js
@@ -68,9 +68,8 @@ export const getAllTeams = async (req, res) => {
     res.status(200).json(JSON.stringify(teams));
   } catch (err) {
     console.error(err);
-    res
-      .status(500)
-      .json({ message: "Internal Server Error", err: err.message });
+    const error = JSON.stringify(err);
+    res.status(400).json({ message: "Something went wrong", error: error });
   }
 };
 
